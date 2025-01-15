8dc603b763aae74f22b9434af1008a87b9ea0478
DachepallySahithi <130071155+DachepallySahithi@users.noreply.github.com>
2025-01-15 11:46:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+function stricpArticle(band) {
+	const articles = ['a', 'an', 'the'];
+	const bandWords = band.split(' ');
+	if (articles.includes(bandWords[0].toLowerCase())){
+		bandWords.shift();
+	}
+	return bandWords.join(' ');
+	}
+const sortedBands = bands.sort((a,b) => {
+	const stripA = stricpArticle(a);
+	const stripB = stricpArticle(b);
+	return stripA.localCompare(stripB);
+});
+const ul = document.getElementById('band');
+    sortedBands.forEach(band => {
+      const li = document.createElement('li');
+      li.textContent = band;
+      ul.appendChild(li);
+});
