63d401265de377d70ac99d74f3ea5baeefa51f32
DachepallySahithi <130071155+DachepallySahithi@users.noreply.github.com>
2025-01-18 15:32:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,38 @@
-function sortBands(bands) {
-            return bands.sort((a, b) => {
-                const stripArticle = name => {
-                    return name.replace(/^(a |an |the )/i, '').toLowerCase();
-                };
+const bands = [
+  'The Plot in You',
+  'The Devil Wears Prada',
+  'Pierce the Veil',
+  'Norma Jean',
+  'The Bled',
+  'Say Anything',
+  'The Midway State',
+  'We Came as Romans',
+  'Counterparts',
+  'Oh, Sleeper',
+  'A Skylit Drive',
+  'Anywhere But Here',
+  'An Old Dog'
+];
 
-                return stripArticle(a).localeCompare(stripArticle(b));
-            });
-        }
+function removeArticles(str) {
+  const articles = ['a', 'an', 'the'];
+  for (const article of articles) {
+    if (str.toLowerCase().startsWith(article + ' ')) {
+      str = str.substring(article.length + 1);
+    }
+  }
+  return str;
+}
 
-        const sortedBands = sortBands(bands);
-        const bandList = document.getElementById('band');
-        sortedBands.forEach(band => {
-            const li = document.createElement('li');
-            li.textContent = band;
-            bandList.appendChild(li);
-        });
+const sortedBands = bands.sort((a, b) => {
+  const aWithoutArticles = removeArticles(a);
+  const bWithoutArticles = removeArticles(b);
+  return aWithoutArticles.localeCompare(bWithoutArticles);
+});
+
+const bandList = document.getElementById('band');
+sortedBands.forEach(band => {
+  const listItem = document.createElement('li');
+  listItem.textContent = band;
+  bandList.appendChild(listItem);
+});
